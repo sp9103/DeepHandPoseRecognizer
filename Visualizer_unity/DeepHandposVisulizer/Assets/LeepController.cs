04c93432ff40fe81bbf4565fb1182143e38b9835
Moon Sungphill <sp9103@naver.com>
2016-09-30 16:29:18 +0900

    Slider controll script add - for visualize probability

--- Visualizer_unity/DeepHandposVisulizer/Assets/LeepController.cs
+++ Visualizer_unity/DeepHandposVisulizer/Assets/LeepController.cs
@@ -2,20 +2,34 @@
 using System.Collections;
 using System.Runtime.InteropServices;
 using System;
+using UnityEngine.UI;
 
 public class LeepController : MonoBehaviour {
 
     [DllImport("LeepDll")]
     private static extern void TEST();
+    [DllImport("LeepDll")]
+    private static extern void Init();
+    [DllImport("LeepDll")]
+    private static extern void CalcProbabilty(float[] prob);
 
     public GameObject HandModel;
+    public Slider[] probSlider = new Slider[10];
+
+    //GEST_COUNT
+    float[] prob = new float[10];
 	// Use this for initialization
 	void Start () {
-        //TEST();
+        Init();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        CalcProbabilty(prob);
+        for(int i = 0; i < 10; i++)
+        {
+            probSlider[i].value = prob[i];
+        }
 	}
 }
--- Visualizer_unity/DeepHandposVisulizer/DllProject/LeepDll/LeepDll/dllMain.cpp
+++ Visualizer_unity/DeepHandposVisulizer/DllProject/LeepDll/LeepDll/dllMain.cpp
@@ -3,6 +3,7 @@
 #include <Windows.h>
 
 #define EXPORT_API __declspec(dllexport)
+#define GEST_COUNT 10
 
 using namespace std;
 
@@ -10,6 +11,21 @@ extern "C"{
 
 	void EXPORT_API TEST(){
 		MessageBox(NULL, L"Client DLL Test Successs", L"TEST", MB_OK);
+	}
+
+	void EXPORT_API Init(){
+
+	}
+
+	void EXPORT_API CalcProbabilty(float prob[]){
+		//TO-DO
+
+		for (int i = 0; i < GEST_COUNT; i++){
+			if (i == 3)
+				prob[i] = 1.0f;
+			else
+				prob[i] = 0.0f;
+		}
 	}
 
 }

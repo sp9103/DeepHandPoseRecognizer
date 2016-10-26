f2ba1e70afa50697b7e57d4995b6a919206a6d32
Moon Sungphill <sp9103@naver.com>
2016-10-26 14:46:45 +0900

    Leap pretrain data loader implement

---
+++ Caffe/windows/pretrain_test/Leap_pretrain_data_loader.cpp
@@ -0,0 +1,88 @@
+#include "Leap_pretrain_data_loader.h"
+
+
+Leap_pretrain_data_loader::Leap_pretrain_data_loader()
+{
+}
+
+
+Leap_pretrain_data_loader::~Leap_pretrain_data_loader()
+{
+}
+
+void Leap_pretrain_data_loader::LoadDataAll(char *datapath){
+	WIN32_FIND_DATA ffd;
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+	TCHAR szDir[MAX_PATH] = { 0, };
+
+	MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, datapath, strlen(datapath), szDir, MAX_PATH);
+	StringCchCat(szDir, MAX_PATH, TEXT("\\*"));
+
+	hFind = FindFirstFile(szDir, &ffd);
+	while (FindNextFile(hFind, &ffd) != 0){
+		TCHAR subDir[MAX_PATH] = { 0, };
+		memcpy(subDir, szDir, sizeof(TCHAR)*MAX_PATH);
+		size_t len;
+		StringCchLength(subDir, MAX_PATH, &len);
+		subDir[len - 1] = '\0';
+		StringCchCat(subDir, MAX_PATH, ffd.cFileName);
+		char tBuf[MAX_PATH];
+		WideCharToMultiByte(CP_ACP, 0, subDir, MAX_PATH, tBuf, MAX_PATH, NULL, NULL);
+
+		//Tchar to char
+		char ccFileName[256];
+		WideCharToMultiByte(CP_ACP, 0, ffd.cFileName, len, ccFileName, 256, NULL, NULL);
+		printf("directory : %s load.\n", ccFileName);
+
+		if (ccFileName[0] != '.'){
+			WIN32_FIND_DATA class_ffd;
+			TCHAR szProcDir[MAX_PATH] = { 0, };
+			HANDLE hDataFind = INVALID_HANDLE_VALUE;
+
+			int idx = 0;
+			char handFile[256];
+			sprintf(handFile, "%s\\%s\\handsData.dat", datapath, ccFileName);
+			FILE *fp = fopen(handFile, "rb");
+
+			while (!feof(fp)){
+				HandData tempHandData[2], targetData;
+				fread(tempHandData, sizeof(HandData), 2, fp);
+				for (int lridx = 0; lridx < 2; lridx++){
+					targetData = tempHandData[lridx];
+					if (tempHandData[lridx].state != 0 && tempHandData[lridx].isLeft == false)
+						break;
+				}
+				if (targetData.state != 0){
+					FilePath tempPath;
+					char file_left[256], file_right[256];
+					int img_idx = targetData.data_counter;
+					sprintf(file_left, "%s\\%s\\Resize\\%d_%d.jpg", datapath, ccFileName, 0, img_idx);
+					sprintf(file_right, "%s\\%s\\Resize\\%d_%d.jpg", datapath, ccFileName, 1, img_idx);
+					tempPath.left_path = file_left;
+					tempPath.right_path = file_right;
+
+					for (int i = 0; i < 5; i++)
+						for (int j = 0; j < 4; j++)
+							for (int k = 0; k < 3; k++)
+								tempPath.fingerJoint[i][j][k] = targetData.finger[i].bone[j][1][k] / 10.f;
+
+					dataList.push_back(tempPath);
+				}
+			}
+			fclose(fp);
+		}
+	}
+}
+
+int Leap_pretrain_data_loader::getCount(){
+	return dataList.size();
+}
+
+void Leap_pretrain_data_loader::getData(cv::Mat *left, cv::Mat *right, FilePath *data, int idx){
+	FilePath targetPath = dataList.at(idx);
+
+	*left = cv::imread(targetPath.left_path, CV_LOAD_IMAGE_GRAYSCALE);
+	*right = cv::imread(targetPath.right_path, CV_LOAD_IMAGE_GRAYSCALE);
+
+	*data = targetPath;
+}
---
+++ Caffe/windows/pretrain_test/Leap_pretrain_data_loader.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <opencv2\opencv.hpp>
+#include <Windows.h>
+#include <strsafe.h>
+#include <conio.h>
+
+struct FingerData
+{
+	int finger_type = NULL;
+	int finger_id = NULL;
+	float finger_length = NULL;
+	float finger_width = NULL;
+
+	float bone[4][3][3];// prevJoint, nextJoint, direction
+};
+struct HandData
+{
+	int data_counter = NULL;
+	int state = 0;
+	bool isLeft = NULL;
+	int hand_id = NULL;
+
+	float hand_palmPosition[3];
+	float hand_palmNormal[3];
+	float hand_direction[3];
+
+	float arm_direction[3];
+	float arm_wristPosition[3];
+	float arm_elbowPosition[3];
+
+	FingerData finger[5];
+};
+
+class Leap_pretrain_data_loader
+{
+public:
+	Leap_pretrain_data_loader();
+	~Leap_pretrain_data_loader();
+
+	void LoadDataAll(char *path);
+	int getCount();
+
+private:
+	typedef struct path_{
+		std::string left_path;
+		std::string right_path;
+		float fingerJoint[5][4][3];
+		int id;
+	}FilePath;
+
+	std::vector<FilePath> dataList;
+
+public:
+	void getData(cv::Mat *left, cv::Mat *right, FilePath *data, int idx);
+};
+

bb595f542251612e86d2eefa8d252d326bfa184c
Moon Sungphill <sp9103@naver.com>
2016-10-26 18:53:52 +0900

    single image deploy protofile commit

--- Caffe/windows/pretrain_test/main.cpp
+++ Caffe/windows/pretrain_test/main.cpp
@@ -1,7 +1,14 @@
 #include <caffe\caffe.hpp>
 
-#define SOLVER "deploy_val.prototxt"
-#define TRAINRESULT "..\\caffe\\IK_AlexNet\\snapshot_with_bn\\IK_AlexNet_iter_85035.caffemodel"
+#include "Leap_pretrain_data_loader.h"
+
+//STEREO
+#define SOLVER "..\\caffe\\resnet_pretrain\\resnet_pretrain_18_deploy.prototxt"
+#define TRAINRESULT "..\\caffe\\resnet_pretrain\\snapshot_pretrain\\resnet_pretrain_18_iter_200000.caffemodel"
+
+//SINGLE
+#define SOLVER "..\\caffe\\resnet_pretrain_single\\resnet_pretrain_18_single_deploy.prototxt"
+#define TRAINRESULT "..\\caffe\\resnet_pretrain_single\\snapshot_pretrain\\resnet_pretrain_18_single_iter_200000.caffemodel"
 
 using namespace caffe;
 
@@ -18,6 +25,17 @@ int main(int argc, char** argv) {
 
 	// caffemodel(weight)
 	caffe_test_net.CopyTrainedLayersFrom(TRAINRESULT);
+
+	Leap_pretrain_data_loader dataLoader;
+	dataLoader.LoadDataAll("M:\\HandData\\TEST");
+
+	float totalDist = 0.0f;
+	float ThumbDist = 0.0f, IndexDist = 0.0f, MiddleDist = 0.0f, RingDist = 0.0f, PinkyDist = 0.0f;
+	for (int i = 0; i < dataLoader.getCount(); i++){
+
+	}
+
+	//Ελ°θ
 
 	return 0;
 }
